335b464498a7f949d5d870859e529ebd30341316
thebanik <45137555+thebanik@users.noreply.github.com>
2021-12-30 15:57:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+var mouseEvent= "empty";
+var last_position_of_x, last_position_of_y;
+canvas =document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="black";
+width_of_line=1;
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown() {
+mouseEvent= "mouseDown";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup() {
+mouseEvent= "mouseup";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave() {
+mouseEvent= "mouseleave";
+}
+canvas.addEventListener("mousemove", my_mousemove)
+function my_mouseMove()
+{
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft; current_position_of_mouse_y = e.clientY - canvas.offsetTop; if (mouseEvent == "mouseDown") { ctx.beginPath(); ctx.strokeStyle = color; ctx.lineWidth = width_of_line; ctx.moveTo(last_position_of_x,
+        last_position_of_y); ctx.lineTo(current_position_of_mouse_x,
+        current_position_of_mouse_y); ctx.stroke();
+        } last_position_of_x = current_position_of_mouse_x; last_position_of_y = current_position_of_mouse_y; }
+
